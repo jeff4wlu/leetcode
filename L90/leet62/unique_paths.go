c45882df2e0de2975f8aca6d9addf2211c69cee2
lwj <jeff_lu@data-spark.cn>
2019-12-31 11:11:55 +0800

    leet62-DP

--- L90/leet62/unique_paths.go
+++ L90/leet62/unique_paths.go
@@ -34,3 +34,30 @@ func solve(rows, cols, i, j int, direction string, path []string, res *[][]strin
 }
 
 //TODO:使用DP
+//首先找出状态所依靠的变量，此题为坐标i,j。
+//其次找出状态转换方程，numsWay[i][j] = numsWay[i-1][j]+numsWay[i][j-1]
+func UniquePaths2(rows, cols int) int {
+
+	//初始化一个二维数组，用作记录过程。空间换时间
+	numsWay := [][]int{}
+	for i := 0; i < rows; i++ {
+		r := make([]int, cols)
+		numsWay = append(numsWay, r)
+	}
+
+	numsWay[0][0] = 1
+	for i := 1; i < cols; i++ {
+		numsWay[0][i] = 1
+	}
+	for i := 1; i < rows; i++ {
+		numsWay[i][0] = 1
+	}
+
+	for i := 1; i < rows; i++ {
+		for j := 1; j < cols; j++ {
+			numsWay[i][j] = numsWay[i-1][j] + numsWay[i][j-1]
+		}
+	}
+
+	return numsWay[rows-1][cols-1]
+}
--- L90/leet62/unique_paths_test.go
+++ L90/leet62/unique_paths_test.go
@@ -34,4 +34,29 @@ func TestUniquePaths(t *testing.T) {
 
 	})
 
+	Convey("TestUniquePaths2", t, func() {
+		Convey("用例1", func() {
+
+			got := UniquePaths2(2, 3)
+			want := 3
+
+			if got != want {
+				t.Errorf("failed")
+			}
+
+		})
+
+		Convey("用例2", func() {
+
+			got := UniquePaths2(7, 3)
+			want := 28
+
+			if got != want {
+				t.Errorf("failed")
+			}
+
+		})
+
+	})
+
 }
