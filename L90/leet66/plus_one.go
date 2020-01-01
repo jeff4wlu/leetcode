80e18f8e9c451a1c5c6c2d65c6b5033f89ca6bdb
jeff4wlu <jeff4w@hotmail.com>
2020-01-01 13:02:56 +0800

    leet66改进

--- L90/leet66/plus_one.go
+++ L90/leet66/plus_one.go
@@ -1,44 +1,25 @@
 package leet66
 
+//因为只加一，所以可以利用9来判断进位
 func PlusOne(num []int) []int {
+
 	n := len(num)
 
-	if n < 1 {
-		return nil
-	}
-
-	var adv, total int
-	res := []int{}
-
-	total = num[n-1] + 1
-	if total >= 10 {
-		adv = 1
-		res = append(res, total-10)
-	} else {
-		res = append(res, total)
-	}
-	i := n - 2
-
-	for ; i >= 0; i-- {
-
-		total = num[i] + adv
-		if total >= 10 {
-			adv = 1
-			tmp := []int{total - 10}
-			res = append(tmp, res...)
+	for i := n - 1; i >= 0; i-- {
+		if num[i] == 9 {
+			num[i] = 0
 		} else {
-			tmp := []int{total}
-			res = append(tmp, res...)
+			num[i]++
 			break
 		}
-
 	}
-	i--
 
-	if i >= 0 {
-		res = append(num[:i+1], res...)
-	} else if i < 0 && adv == 1 {
-		res = append([]int{1}, res...)
+	res := []int{}
+	if num[0] == 0 {
+		res = append([]int{1}, num...)
+	} else {
+		res = append(res, num...)
 	}
+
 	return res
 }
