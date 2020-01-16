9d3ac9c48e5caf91451584b7659bf6ce22ab1554
lwj <jeff_lu@data-spark.cn>
2020-01-16 15:24:26 +0800

    完善53

--- L60/leet53/max_subarr.go
+++ L60/leet53/max_subarr.go
@@ -2,75 +2,91 @@ package leet53
 
 import "leetcode/infra"
 
-//DP
-func MaxSubarr(nums []int) int {
+//暴力破解,复杂度为O(n2)
+func MaxSubarr1(in []int) int {
 
-	n := len(nums)
-	dp := make([]int, n)
-	dp[0] = 0
-	ans := dp[0]
-	for i := 1; i < n; i++ {
-		if dp[i-1] > 0 {
-			dp[i] = dp[i-1] + nums[i]
-		} else {
-			dp[i] = nums[i]
-		}
-		ans = infra.IntMax(ans, dp[i])
-
+	n := len(in)
+	if n == 0 {
+		return 0
 	}
 
-	return ans
-}
-
-//分治
-func MaxSubarr2(nums []int) int {
-
-	return findMaxSub(nums, 0, len(nums)-1)
-}
-
-func findMaxSub(nums []int, low, high int) int {
-
-	if low == high {
-		return nums[low]
+	if n == 1 {
+		return in[0]
 	}
 
-	mid := (high + low) / 2
-
-	leftMax := findMaxSub(nums, low, mid)
-	rightMax := findMaxSub(nums, mid+1, high)
-	crossMax := findMaxCrossSub(nums, low, mid, high)
-
-	if leftMax >= rightMax && leftMax >= crossMax {
-		return leftMax
-	} else if rightMax >= leftMax && rightMax >= crossMax {
-		return rightMax
-	}
-
-	return crossMax
-}
-
-func findMaxCrossSub(nums []int, low, mid, high int) int {
-
-	maxLeft := infra.INT32_MIN
-	maxRight := infra.INT32_MIN
-	var sum int
-
-	for i := mid; i >= low; i-- {
-		sum += nums[i]
-		if sum > maxLeft {
-			maxLeft = sum
-			//left = i
+	max := infra.INT32_MIN
+	for i := 0; i < n; i++ {
+		max = infra.IntMax(max, in[i])
+		tmp := in[i]
+		for j := i + 1; j < n; j++ {
+			tmp += in[j]
+			max = infra.IntMax(max, tmp)
 		}
 	}
 
-	sum = 0
-	for i := mid + 1; i <= high; i++ {
-		sum += nums[i]
-		if sum > maxRight {
-			maxRight = sum
-			//right = i
-		}
+	return max
+}
+
+//DP,复杂度是O(n)
+func MaxSubarr2(in []int) int {
+
+	n := len(in)
+	if n == 0 {
+		return 0
 	}
 
-	return maxLeft + maxRight
+	if n == 1 {
+		return in[0]
+	}
+
+	max := infra.INT32_MIN
+	sum := 0
+	for i := 0; i < n; i++ {
+		if sum < 0 {
+			sum = 0
+		}
+		sum += in[i]
+		max = infra.IntMax(sum, max)
+
+	}
+
+	return max
+}
+
+//分治，列举出所有可能性，逐步分解问题。最后汇总。
+func MaxSubarr3(in []int) int {
+	return helper(in)
+}
+
+func helper(in []int) int {
+
+	n := len(in)
+	if n == 0 {
+		return infra.INT32_MIN //注意这里不是0
+	}
+	if n == 1 {
+		return in[0]
+	}
+
+	mid := (n+1)/2 - 1 //求中间元素的索引值
+
+	left := helper(in[:mid])
+	right := helper(in[mid+1:])
+
+	max := in[mid]
+	sum := in[mid]
+	for i := mid - 1; i >= 0; i-- {
+		sum += in[i]
+		max = infra.IntMax(sum, max)
+	}
+	sum = max //此句很关键
+	for i := mid + 1; i < n; i++ {
+		sum += in[i]
+		max = infra.IntMax(sum, max)
+	}
+	max = infra.IntMax(left, max)
+	max = infra.IntMax(right, max)
+
+	return max
+
 }
--- L60/leet53/max_subarr_test.go
+++ L60/leet53/max_subarr_test.go
@@ -11,7 +11,7 @@ func TestMaxSubarr(t *testing.T) {
 	Convey("TestMaxSubarr", t, func() {
 		Convey("1", func() {
 			nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
-			got := MaxSubarr(nums)
+			got := MaxSubarr1(nums)
 			want := 6
 
 			if got != want {
@@ -26,6 +26,20 @@ func TestMaxSubarr(t *testing.T) {
 		Convey("1", func() {
 			nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 			got := MaxSubarr2(nums)
+			want := 6
+
+			if got != want {
+				t.Errorf("got %d, want  %d", got, want)
+			}
+
+		})
+
+	})
+
+	Convey("TestMaxSubarr3", t, func() {
+		Convey("1", func() {
+			nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+			got := MaxSubarr3(nums)
 			want := 6
 
 			if got != want {
