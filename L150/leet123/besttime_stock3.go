cb34377f1ef832d4dcd5a9f8161588b684596d27
jeff4wlu <jeff4w@hotmail.com>
2020-02-03 21:09:03 +0800

    leet123

--- L150/leet123/besttime_stock3.go
+++ L150/leet123/besttime_stock3.go
@@ -33,3 +33,49 @@ func BestTimeStock31(in []int) int {
 	}
 	return max
 }
+
+//DP
+//题意是最多2次交易的最大获利值，可以0，1，2次交易
+//所以分成5个阶段，1，3，5是买入，卖出/买入，卖出。2，4是持有股票。每个阶段又分两种情况，如下
+func BestTimeStock32(prices []int) int {
+	n := len(prices)
+
+	//i表示天数，j表示阶段。最多交易两次被表示为5个阶段
+	//dp[i][j]代表前i天第j阶段获得的最大收益
+	dp := [][]int{}
+	for i := 0; i <= n; i++ {
+		tmp := make([]int, 5+1)
+		dp = append(dp, tmp)
+	}
+
+	for i := 1; i <= n; i++ {
+		//phase1,3,5。现在手头没股票
+		for j := 1; j <= 5; j += 2 {
+			//前一天也没有股票
+			dp[i][j] = dp[i-1][j]
+			//前一天有股票，今天卖了。求前一天没有股票情况下的收益大还是前一天有股票情况下收益大
+			//前一天有股票，证明不是第一阶段，所以j>1
+			if i > 1 && j > 1 {
+				dp[i][j] = infra.IntMax(dp[i][j], dp[i-1][j-1]+prices[i-1]-prices[i-2]) //prices是0base的
+			}
+
+		}
+		//phase2,4。现在手头有股票
+		for j := 2; j <= 5; j += 2 {
+			//前一天无股票，今天买了股票
+			dp[i][j] = dp[i-1][j-1]
+			//前一天有股票，今天继续持有
+			if i > 1 {
+				dp[i][j] = infra.IntMax(dp[i][j], dp[i-1][j-1]+prices[i-1]-prices[i-2]) //prices是0base的
+			}
+
+		}
+	}
+
+	var res int
+	for j := 1; j <= 5; j++ {
+		res = infra.IntMax(res, infra.IntMax(0, dp[n][j]))
+	}
+
+	return res
+}
--- L150/leet123/besttime_stock3_test.go
+++ L150/leet123/besttime_stock3_test.go
@@ -21,4 +21,37 @@ func TestBestTimeStock31(t *testing.T) {
 
 	})
 
+	Convey("TestBestTimeStock32", t, func() {
+		Convey("1", func() {
+			got := BestTimeStock32([]int{3, 3, 5, 0, 0, 3, 1, 4})
+			want := 6
+
+			if got != want {
+				t.Errorf("got %d, want  %d", got, want)
+			}
+
+		})
+
+		Convey("2", func() {
+			got := BestTimeStock32([]int{1, 2, 3, 4, 5})
+			want := 4
+
+			if got != want {
+				t.Errorf("got %d, want  %d", got, want)
+			}
+
+		})
+
+		Convey("3", func() {
+			got := BestTimeStock32([]int{7, 6, 4, 3, 1})
+			want := 0
+
+			if got != want {
+				t.Errorf("got %d, want  %d", got, want)
+			}
+
+		})
+
+	})
+
 }
