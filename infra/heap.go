bf6e19306a415b7cd88f697c4c66040659ee6f3c
jeff4wlu <jeff4w@hotmail.com>
2020-02-09 13:46:15 +0800

    heapsort

--- infra/heap.go
+++ infra/heap.go
@@ -1,5 +1,6 @@
 package infra
 
+//heap有两种构建方式，一种是对一个已有树构建堆，另外是边读取边构建
 type Heap struct {
 	tree  []int
 	size  int
@@ -7,9 +8,16 @@ type Heap struct {
 }
 
 //isTop是true时小顶堆
-func NewHeap(nums []int, isTop bool) *Heap {
+func NewHeap1(nums []int, isTop bool) *Heap {
 	h := new(Heap)
 	h.init(nums, isTop)
+	return h
+}
+
+func NewHeap2(isTop bool) *Heap {
+	h := new(Heap)
+	h.size = 0
+	h.isTop = isTop
 	return h
 }
 
@@ -20,14 +28,14 @@ func (h *Heap) init(nums []int, isTop bool) {
 	copy(h.tree, nums)
 
 	if h.isTop {
-		h.buildTopHeap()
+		h.BuildTopHeap()
 	} else {
-		h.buildBtmHeap()
+		h.BuildBtmHeap()
 	}
 }
 
 //小顶堆
-func (h *Heap) buildTopHeap() {
+func (h *Heap) BuildTopHeap() {
 	//求最后一个非叶子结点(0base)
 	for i := h.size/2 - 1; i >= 0; i-- {
 		h.adjustTopHeap(i)
@@ -35,7 +43,7 @@ func (h *Heap) buildTopHeap() {
 }
 
 //大顶堆
-func (h *Heap) buildBtmHeap() {
+func (h *Heap) BuildBtmHeap() {
 	//求最后一个非叶子结点(0base)
 	for i := h.size/2 - 1; i >= 0; i-- {
 		h.adjustBtmHeap(i)
@@ -70,4 +78,74 @@ func (h *Heap) adjustBtmHeap(node int) {
 		h.adjustBtmHeap(k) //由于交换后可能又导致子树不符合堆的条件，所以要调整子树
 	}
 
+}
+
+//0base。必须使用它来获取父亲结点，否则返回不了负数
+func (h *Heap) parent(idx int) int {
+
+	if idx == 0 {
+		return -1
+	}
+
+	return (idx - 1) / 2
+}
+
+//本方法适合处理流数据的优先值
+func (h *Heap) TopInsert(num int) {
+
+	h.tree = append(h.tree, num)
+	h.size++
+
+	parent := h.parent(h.size - 1)
+	for parent >= 0 {
+		k := parent*2 + 1
+		if k < h.size {
+			if k+1 < h.size && h.tree[k] > h.tree[k+1] {
+				k++
+			}
+			if h.tree[parent] > h.tree[k] {
+				h.tree[parent], h.tree[k] = h.tree[k], h.tree[parent]
+			}
+		}
+
+		parent = h.parent(parent)
+	}
+}
+
+func (h *Heap) BtmInsert(num int) {
+
+	h.tree = append(h.tree, num)
+	h.size++
+
+	parent := h.parent(h.size - 1)
+	for parent >= 0 {
+		k := parent*2 + 1
+		if k < h.size {
+			if k+1 < h.size && h.tree[k] < h.tree[k+1] {
+				k++
+			}
+			if h.tree[parent] < h.tree[k] {
+				h.tree[parent], h.tree[k] = h.tree[k], h.tree[parent]
+			}
+		}
+
+		parent = h.parent(parent)
+	}
+}
+
+func (h *Heap) HeapSort(isTop bool) []int {
+
+	res := []int{}
+	for h.size > 0 {
+		res = append(res, h.tree[0])
+		h.tree[0] = h.tree[h.size-1]
+		h.size--
+		if isTop {
+			h.BuildTopHeap()
+		} else {
+			h.BuildBtmHeap()
+		}
+	}
+
+	return res
 }
--- infra/heap_test.go
+++ infra/heap_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestHeap(t *testing.T) {
 
-	Convey("TestTopHeap", t, func() {
-		Convey("用例1", func() {
+	Convey("TestNewHeap1", t, func() {
+		Convey("构建小顶堆", func() {
 
-			got := NewHeap([]int{3, 9, 20, 15, 7}, true)
-			want := []int{3, 7, 20, 15, 9}
+			got := NewHeap1([]int{9, 3, 7, 6, 5, 1, 10, 2}, true)
+			want := []int{1, 2, 7, 3, 5, 9, 10, 6}
 
 			if !IntArrSeqCmp(got.tree, want) {
 				t.Errorf("failed")
@@ -20,12 +20,9 @@ func TestHeap(t *testing.T) {
 
 		})
 
-	})
+		Convey("构建大顶堆", func() {
 
-	Convey("TestBtmHeap", t, func() {
-		Convey("用例1", func() {
-
-			got := NewHeap([]int{9, 4, 2, 3, 5, 1, 8, 6, 7, 0}, false)
+			got := NewHeap1([]int{9, 4, 2, 3, 5, 1, 8, 6, 7, 0}, false)
 			want := []int{9, 7, 8, 6, 5, 1, 2, 4, 3, 0}
 
 			if !IntArrSeqCmp(got.tree, want) {
@@ -36,4 +33,75 @@ func TestHeap(t *testing.T) {
 
 	})
 
+	Convey("TestNewHeap2", t, func() {
+		Convey("构建小顶堆", func() {
+
+			h := NewHeap2(true)
+			arr := []int{9, 3, 7, 6, 5, 1, 10, 2}
+			for i := 0; i < len(arr); i++ {
+				h.TopInsert(arr[i])
+			}
+			/*
+				got := h.tree
+				want := []int{1, 2, 7, 3, 5, 9, 10, 6}
+
+				if !IntArrSeqCmp(got, want) {
+					t.Errorf("failed")
+				}*/
+
+		})
+
+		Convey("构建大顶堆", func() {
+
+			h := NewHeap2(false)
+			arr := []int{9, 4, 2, 3, 5, 1, 8, 6, 7, 0}
+			for i := 0; i < len(arr); i++ {
+				h.BtmInsert(arr[i])
+			}
+			/*
+				got := h.tree
+				want := []int{9, 7, 8, 6, 5, 1, 2, 4, 3, 0}
+
+				if !IntArrSeqCmp(got, want) {
+					t.Errorf("failed")
+				}*/
+
+		})
+
+	})
+
+	Convey("TestHeapSort", t, func() {
+		Convey("小顶堆排序", func() {
+
+			h := NewHeap2(true)
+			arr := []int{9, 3, 7, 6, 5, 1, 10, 2}
+			for i := 0; i < len(arr); i++ {
+				h.TopInsert(arr[i])
+			}
+			got := h.HeapSort(true)
+			want := []int{1, 2, 3, 5, 6, 7, 9, 10}
+
+			if !IntArrSeqCmp(got, want) {
+				t.Errorf("failed")
+			}
+
+		})
+
+		Convey("大顶堆排序", func() {
+
+			h := NewHeap2(false)
+			arr := []int{9, 4, 2, 3, 5, 1, 8, 6, 7, 0}
+			for i := 0; i < len(arr); i++ {
+				h.BtmInsert(arr[i])
+			}
+			got := h.HeapSort(false)
+			want := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+
+			if !IntArrSeqCmp(got, want) {
+				t.Errorf("failed")
+			}
+
+		})
+
+	})
 }
