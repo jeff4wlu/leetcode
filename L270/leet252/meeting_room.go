de62a91ea2f4db99f601195ddf0aa1f63c94b756
jeff4wlu <jeff4w@hotmail.com>
2020-02-13 00:03:51 +0800

    252

--- L270/leet252/meeting_room.go
+++ L270/leet252/meeting_room.go
@@ -11,23 +11,27 @@ func MeetingRoom(ranges [][]int) bool {
 	start := ranges[0][0]
 	end := ranges[0][1]
 	for i := 1; i < times; i++ {
-		if start == ranges[i][0] {
-			return false
-		} else if start-ranges[i][0] < 0 && end-ranges[i][0] > 0 {
-			return false
-		} else if start-ranges[i][0] > 0 && start-ranges[i][1] < 0 {
+		if !IsOverlap([]int{start, end}, ranges[i]) {
 			return false
 		}
 		start, end = ranges[i][0], ranges[i][1]
 	}
 
-	if start == ranges[0][0] {
-		return false
-	} else if start-ranges[0][0] < 0 && end-ranges[0][0] > 0 {
-		return false
-	} else if start-ranges[0][0] > 0 && start-ranges[0][1] < 0 {
+	if !IsOverlap([]int{start, end}, ranges[0]) {
 		return false
 	}
 
+	return true
+}
+
+func IsOverlap(a, b []int) bool {
+
+	if a[0] == b[0] {
+		return false
+	} else if a[0]-b[0] < 0 && a[1]-b[0] > 0 {
+		return false
+	} else if a[0]-b[0] > 0 && a[0]-b[1] < 0 {
+		return false
+	}
 	return true
 }
