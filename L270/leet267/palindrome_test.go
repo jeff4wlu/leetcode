80b4b6b0556b91d354bfd3e9006552681c84590e
jeff4wlu <jeff4w@hotmail.com>
2020-02-16 09:32:23 +0800

    267

--- L270/leet267/palindrome_test.go
+++ L270/leet267/palindrome_test.go
@@ -11,10 +11,53 @@ func TestPalindrome(t *testing.T) {
 
 	Convey("TestPalindrome", t, func() {
 
-		Convey("1", func() {
+		Convey("回文偶数", func() {
 
 			got := Palindrome("aabb")
 			want := []string{"abba", "baab"}
+
+			if !infra.StringArrComp(got, want) {
+				t.Errorf("failed")
+			}
+
+		})
+
+		Convey("非回文", func() {
+
+			got := Palindrome("code")
+			want := []string{}
+
+			if !infra.StringArrComp(got, want) {
+				t.Errorf("failed")
+			}
+
+		})
+
+		Convey("一个字符", func() {
+
+			got := Palindrome("c")
+			want := []string{"c"}
+
+			if !infra.StringArrComp(got, want) {
+				t.Errorf("failed")
+			}
+
+		})
+
+		Convey("回文基数", func() {
+
+			got := Palindrome("aaabb")
+			want := []string{"ababa", "baaab"}
+
+			if !infra.StringArrComp(got, want) {
+				t.Errorf("failed")
+			}
+
+		})
+		Convey("有重复字符的回文", func() {
+
+			got := Palindrome("aabaaab")
+			want := []string{"aababaa", "abaaaba", "baaaaab"}
 
 			if !infra.StringArrComp(got, want) {
 				t.Errorf("failed")
