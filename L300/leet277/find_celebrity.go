aa4fd4ccd7f75b262c65933401f76c2eda917d8c
jeff4wlu <jeff4w@hotmail.com>
2020-02-17 19:57:23 +0800

    277

--- L300/leet277/find_celebrity.go
+++ L300/leet277/find_celebrity.go
@@ -1,6 +1,6 @@
 package leet277
 
-//使用图的出度和入度
+//使用图的出度和入度，O(n2)效率不高
 func FindCelebrity(relations [][]int) int {
 
 	n := len(relations)
@@ -29,5 +29,31 @@ func FindCelebrity(relations [][]int) int {
 	}
 
 	return -1
+
+}
+
+//根据题意总结出隐含条件，最多只有一个明星；明星不认识任何人。
+//效率为O(n)
+func FindCelebrity1(relations [][]int) int {
+
+	n := len(relations)
+	if n < 2 {
+		return -1
+	}
+
+	var k int
+	for i := 1; i < n; i++ {
+		if relations[i][k] == 0 { //i不认识k
+			k = i
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if k != i && (relations[k][i] == 1 || relations[i][k] == 0) {
+			return -1
+		}
+	}
+
+	return k
 
 }
--- L300/leet277/find_celebrity_test.go
+++ L300/leet277/find_celebrity_test.go
@@ -47,3 +47,45 @@ func TestFindCelebrity(t *testing.T) {
 	})
 
 }
+
+func TestFindCelebrity1(t *testing.T) {
+
+	Convey("TestFindCelebrity1", t, func() {
+
+		Convey("1", func() {
+
+			relations := [][]int{
+				{1, 1, 0},
+				{0, 1, 0},
+				{1, 1, 1},
+			}
+
+			got := FindCelebrity1(relations)
+			want := 1
+
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+
+		})
+
+		Convey("2", func() {
+
+			relations := [][]int{
+				{1, 0, 1},
+				{1, 1, 0},
+				{0, 1, 1},
+			}
+
+			got := FindCelebrity1(relations)
+			want := -1
+
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+
+		})
+
+	})
+
+}
